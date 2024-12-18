bd360d72ce095a1c4f4e4f2e01ea664cfc7f5ccf
natycg <checon@roverkids.org>
2024-12-18 13:26:24 -0500

    done 3.1

---
+++ SoundAndEffects/Assets/scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
